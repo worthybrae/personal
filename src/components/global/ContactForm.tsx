1b90ff302bc4f72c156d7350f6af6e2c87996dba
worthybrae <worthyrae@gmail.com>
2025-01-28 04:19:05 -0500

    import bug

--- src/components/global/ContactForm.tsx
+++ src/components/global/ContactForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { createClient } from '@supabase/supabase-js';
